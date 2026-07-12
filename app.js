f3a9e97ab3ccedb4da74c6aa66c205ad3dbb3ce3
Kassandra <kam.terbeek@gmail.com>
2026-07-12 17:33:56 -0600

    Add async function to load and display cat data

---
+++ app.js
@@ -0,0 +1,17 @@
+
+async function loadCats() {
+  const container=document.getElementById("catContainer");
+  if(!container) return;
+  try {
+    const response=await fetch("data/cats/json");
+    const cats=await response.json();
+    cats.forEach(cat=> {
+      const card=document.createElement("div");
+      card.className="card";
+      card.innerHTML=`
+      <h3>${cat.name}</h3>
+      <p><strong>Age:</strong> ${cat.age}</p>
+      <p><strong>Weight:</strong> ${cat.weight} lbs</p>
+      <p><strong>Water Today:</strong> ${cat.waterToday} mL </p>
+      
+      
